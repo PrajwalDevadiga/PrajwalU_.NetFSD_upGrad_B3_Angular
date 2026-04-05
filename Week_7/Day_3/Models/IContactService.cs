151c1228f2261fa3997a5bf58e4c8f875cccfa60
PrajwalDevadiga <121891121+PrajwalDevadiga@users.noreply.github.com>
2026-04-06 01:00:59 +0530

    Add files via upload

---
+++ Week_7/Day_3/Models/IContactService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Models;
+
+public interface IContactService
+{
+    List<ContactInfo> GetAllContacts();
+    ContactInfo GetContactById(int id);
+    void AddContact(ContactInfo contact);
+}
