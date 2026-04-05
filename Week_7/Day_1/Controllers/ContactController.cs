084bbff2dc8a42f3e967e8d87a0f53d12d294088
PrajwalDevadiga <121891121+PrajwalDevadiga@users.noreply.github.com>
2026-04-05 23:56:46 +0530

    Add files via upload

---
+++ Week_7/Day_1/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ContactController : Controller
+    {
+        private static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        public ActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+
+        public ActionResult GetContactById(int id)
+        {
+            var contact = contacts.FirstOrDefault(c => c.ContactId == id);
+            return View(contact);
+        }
+
+        public ActionResult AddContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContact(ContactInfo contactInfo)
+        {
+            contacts.Add(contactInfo);
+            return RedirectToAction("ShowContacts");
+        }
+    }
+}
