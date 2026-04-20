cedb3d09e0d3cf09254f4f632f773d69fc70ec49
PrajwalDevadiga <121891121+PrajwalDevadiga@users.noreply.github.com>
2026-04-20 15:35:30 +0530

    Add files via upload

---
+++ Week_8/Day_1/Repository/IContactRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Repository
+{
+    public interface IContactRepository
+    {
+        List<ContactInfo> GetAllContacts();
+        ContactInfo GetContactById(int id);
+        void AddContact(ContactInfo contact);
+        void UpdateContact(ContactInfo contact);
+        void DeleteContact(int id);
+
+        List<Company> GetCompanies();
+        List<Department> GetDepartments();
+    }
+}
