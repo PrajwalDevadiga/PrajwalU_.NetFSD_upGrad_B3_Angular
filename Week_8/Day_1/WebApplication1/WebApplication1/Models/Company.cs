e062217c569a0746835b6a4b022a37ef8eeb5a70
PrajwalDevadiga <121891121+PrajwalDevadiga@users.noreply.github.com>
2026-04-20 15:33:36 +0530

    Delete Week_8/Day_1 directory

--- Week_8/Day_1/WebApplication1/WebApplication1/Models/Company.cs
+++
@@ -1,5 +0,0 @@
-﻿public class Company
-{
-    public int CompanyId { get; set; }
-    public string CompanyName { get; set; }
-}
--- Week_8/Day_1/WebApplication1/WebApplication1/Models/Department.cs
+++
@@ -1,5 +0,0 @@
-﻿public class Department
-{
-    public int DepartmentId { get; set; }
-    public string DepartmentName { get; set; }
-}
